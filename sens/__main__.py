ed36cf83d9dcd4f234ace9fe4daf5c9a8e0afee8
Mark McGuire <mark.b.mcg@gmail.com>
2013-10-06 22:07:17 +0000

    Package module

--- sens/__main__.py
+++ sens/__main__.py
@@ -1,12 +1,3 @@
-import argparse
-import sys
-from sens import image
-from sens import status
+import sens.application
 
-parser = argparse.ArgumentParser(description='Generate twitch status image')
-parser.add_argument('channel_name', metavar='channel-name', help='Channel to pull data from')
-parser.add_argument('file', help='File to write image to')
-args = parser.parse_args()
-s = status.get_status(args.channel_name)
-img = image.build_image(s)
-img.save(args.file, format='png')
+sens.application.main()
---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(name='Sens',
+      version='0.0.1',
+      description='A dynamic Twitch status image generator',
+      author='Mark McGuire',
+      author_email='???',
+      url='https://github.com/TronPaul/sens',
+
+      install_requires=['Pillow>=2.2.1'],
+
+      entry_points={
+          'console_scripts': ['sens = sens.application:main'],
+      },
+      packages=setuptools.find_packages(),
+      zip_safe=True,
+)
