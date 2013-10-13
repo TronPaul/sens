e22fef4b87c495cb97202ad5374ac7c4950a5cac
Mark McGuire <mark.b.mcg@gmail.com>
2013-10-13 15:07:03 +0000

    Add exceptions and twitch error handling

--- sens/status.py
+++ sens/status.py
@@ -2,25 +2,48 @@ import http.client
 import io
 import json
 
+HOSTNAME = 'api.twitch.tv'
+BASE_PATH = '/kraken'
+HEADERS = {'Accept':'application/vnd.twitchtv.v3+json'}
+
+class TwitchError(Exception):
+    def __init__(self, response, *args, **kwargs):
+        super(TwitchError, self).__init__(*args, **kwargs)
+        self.response = response
+
+class ChannelNotFoundError(TwitchError):
+    pass
+
+def get_twitch_path(path):
+    conn = http.client.HTTPSConnection(HOSTNAME)
+    conn.request('GET', path, headers=HEADERS)
+    resp = conn.getresponse()
+    if resp.status == 404:
+        raise ChannelNotFoundError(resp)
+    elif resp.status != 200:
+        raise TwitchError(resp)
+    return json.load(io.TextIOWrapper(resp))
+
+def get_stream(channel_name):
+    path = '{base}/streams/{channel_name}'.format(
+            base=BASE_PATH, channel_name=channel_name)
+    return get_twitch_path(path)
+
+def get_channel(channel_name):
+    path = '{base}/channels/{channel_name}'.format(
+            base=BASE_PATH, channel_name=channel_name)
+    return get_twitch_path(path)
+
 def get_status(channel_name):
-    conn = http.client.HTTPSConnection('api.twitch.tv', strict=False)
-    base_path = '/kraken/'
-    headers = {'Accept':'application/vnd.twitchtv.v3+json'}
-    conn.request('GET', '{base}channels/{channel_name}'.format(
-        base=base_path, channel_name=channel_name), headers=headers)
-    channel_resp = conn.getresponse()
-    channel_data = json.load(io.TextIOWrapper(channel_resp))
-    conn.request('GET', '{base}streams/{channel_name}'.format(
-        base=base_path, channel_name=channel_name), headers=headers)
-    stream_resp = conn.getresponse()
-    stream_data = json.load(io.TextIOWrapper(stream_resp))
+    channel_data = get_channel(channel_name)
+    stream_data = get_stream(channel_name)
     display_name = channel_data['display_name']
     status = channel_data['status']
     game = channel_data['game']
     logo_url = channel_data['logo']
     online = stream_data['stream'] is not None
-    viewers = stream_data['viewers'] if online else None
-    preview_url = stream_data['preview']['small'] if online else None
+    viewers = stream_data['stream']['viewers'] if online else None
+    preview_url = stream_data['stream']['preview']['small'] if online else None
     return TwitchStatus(logo_url, status, display_name, game, online,
                         viewers, preview_url)
 
