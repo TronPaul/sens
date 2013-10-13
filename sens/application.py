5142df604b1492f25bdbcc4a3f572af256d0fe4c
Mark McGuire <mark.b.mcg@gmail.com>
2013-10-13 15:24:38 +0000

    add verbose option

--- sens/application.py
+++ sens/application.py
@@ -5,11 +5,17 @@ import sens.status
 
 def main():
     parser = argparse.ArgumentParser(description='Generate twitch status image')
+    parser.add_argument('-v', '--verbose', action='store_true', help='verbose')
     parser.add_argument('channel_name', metavar='channel-name', help='Channel to pull data from')
     parser.add_argument('file', help='File to write image to')
     args = parser.parse_args()
-    img = build_image(args.channel_name)
-    img.save(args.file, format='png')
+    try:
+        img = build_image(args.channel_name)
+        img.save(args.file, format='png')
+    except sens.status.TwitchError as e:
+        print('Error retrieving channel data from Twitch.tv')
+        if args.verbose:
+            print('Response: {json}'.format(json=e.json), sys.stderr)
 
 def build_image(channel_name):
     s = sens.status.get_status(channel_name)
